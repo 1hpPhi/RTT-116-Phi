bb267596b3f250cf32a907f7b7868da520c74c7b
DESKTOP-QKSUAG7\1hpPhi <d.phitran@gmail.com>
2024-10-07 11:59:41 -0500

    303.4 & 5

---
+++ Module-303/Module-303/src/M303_4/IfStatementsExample.java
@@ -0,0 +1,12 @@
+package M303_4;
+
+public class IfStatementsExample {
+    public static void main(String[] args) {
+
+        int year = 1992;
+
+        if ((year % 4 == 0) && (year % 100 != 0)) {
+            System.out.println("The year is a leap year.");
+        }
+    }
+}
