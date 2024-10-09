a4809ab227efaf83cad052d4a95cb490bfbaa915
DESKTOP-QKSUAG7\1hpPhi <d.phitran@gmail.com>
2024-10-09 10:52:45 -0500

    More homework

--- Module-303/Module-303/src/Homework/Modules/M303_7/PA_303_7_1.java
+++ Module-303/Module-303/src/Homework/Modules/M303_7/PA_303_7_1.java
@@ -17,6 +17,42 @@ public class PA_303_7_1 {
         array3.clone().toString();
 
         //Task 4
+        int[] array4 = new int[5];
+        System.out.println(array4[0]);
+        System.out.println(array4[4]);
 
+        System.out.println(array4.length - 1);
+        System.out.println(array4[0]);
+        System.out.println(array4[4]);
+
+        //Task 5
+        int[] array5 = new int[5];
+        int i = 0;
+
+        while (array5 != null) {
+            System.out.println(array5[i]);
+            array5[i]++;
+        }
+
+        //Task 6
+        int[] array6 = new int[5];
+        int j = 0;
+
+        while (array6 != null) {
+            System.out.println(array6[j * 2]);
+            array6[j]++;
+        }
+
+        //Task 7
+        int[] array7 = new int[5];
+        int k = 0;
+
+        while (array7 != null && k != 2) {
+            System.out.println(array7[k]);
+            array7[k]++;
+        }
+
+        //Task 8
+        
     }
 }
