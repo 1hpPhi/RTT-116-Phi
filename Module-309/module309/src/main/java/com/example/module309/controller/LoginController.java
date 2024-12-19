1b09a1f734784026017d73de36ba0934ca8a39b5
DESKTOP-QKSUAG7\1hpPhi <d.phitran@gmail.com>
2024-12-19 12:04:24 -0600

    updating login security

---
+++ Module-309/module309/src/main/java/com/example/module309/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.example.module309.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Slf4j
+@Controller
+public class LoginController {
+
+    @GetMapping("/login/login")
+    public ModelAndView theLoginPage() {
+        ModelAndView response= new ModelAndView();
+
+        response.setViewName("login/loginPage");
+
+        return response;
+    }
+}
